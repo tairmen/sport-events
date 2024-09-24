54d298c5f22d42e30645b7ad3c9ee6ed24e66439
Tair <abduraimovtair@gmail.com>
2024-09-24 13:20:27 +0300

    update db modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,15 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlaygroundModule } from './playground/playground.module';
 import { UserModule } from './user/user.module';
 import { EventModule } from './event/event.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './config/database.module';
+import { AppConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '172.17.0.3', // Change this to your PostgreSQL server host
-      port: 5432, // Change this to your PostgreSQL server port
-      username: 'postgres', // Change this to your PostgreSQL username
-      password: 'Aa111111', // Change this to your PostgreSQL password
-      database: 'sport-events', // Change this to your PostgreSQL database name
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entity files
-      synchronize: true, // Synchronize the entities with the database (be careful with this in production)
-    }),
+    AppConfigModule, 
+    DatabaseModule,
     PlaygroundModule,
     UserModule,
     EventModule,
